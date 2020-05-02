569c3d0ddb7a6911ff60428879d5d411a205e436
bandoh <kelvinbandoh@hotmail.com>
2020-05-02 22:18:36 +0000

    fix

--- banpost/src/main/java/com/bandoh/banpost/MainApp.java
+++ banpost/src/main/java/com/bandoh/banpost/MainApp.java
@@ -7,12 +7,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class MainApp extends Application {
-
-    private static final Logger log = LoggerFactory.getLogger(MainApp.class);
 
     public static void main(String[] args) throws Exception {
         launch(args);
@@ -20,14 +16,10 @@ public class MainApp extends Application {
 
     public void start(Stage stage) throws Exception {
 
-        log.info("Starting Hello JavaFX and Maven demonstration application");
-
         String fxmlFile = "/fxml/mainWindow.fxml";
-        log.debug("Loading FXML for main view from: {}", fxmlFile);
         FXMLLoader loader = new FXMLLoader();
         Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
 
-        log.debug("Showing JFX scene");
         Scene scene = new Scene(rootNode);
         scene.getStylesheets().add("/styles/styles.css");
 
